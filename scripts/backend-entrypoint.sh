139e12914fb492b43ce611dc4b6cef924813229c
Alyson Vilela <ctt.alysonvilela@gmail.com>
2026-04-28 14:28:54 +0000

    feat: default admin email and password

--- scripts/backend-entrypoint.sh
+++ scripts/backend-entrypoint.sh
@@ -3,4 +3,8 @@ set -e
 
 node_modules/.bin/medusa db:migrate
 
+if [ -n "$MEDUSA_ADMIN_EMAIL" ] && [ -n "$MEDUSA_ADMIN_PASSWORD" ]; then
+  node_modules/.bin/medusa user -e "$MEDUSA_ADMIN_EMAIL" -p "$MEDUSA_ADMIN_PASSWORD" || true
+fi
+
 exec node_modules/.bin/medusa start
